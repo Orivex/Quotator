db59c7d5de5d7507027084416b0e19b0c305f7c1
Blacklight <bilalabdlhamid6@gmail.com>
2025-06-07 17:22:52 +0200

    Animations

--- quotator-app/app/quote.jsx
+++ quotator-app/app/quote.jsx
@@ -1,55 +1,141 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Pressable, Animated, useAnimatedValue } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { Colors } from '@/constants/Colors'
 import { FontFamilies } from '@/constants/FontFamilies'
 import { quoteBoxStyle } from '@/constants/quoteBoxStyle'
 import { useSQLiteContext } from 'expo-sqlite'
 import { useEffect, useState } from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
 
 const Quote = () => {
   const navigation = useNavigation();
 
   const db = useSQLiteContext();
 
-  const [randomQuote, setRandomQuote] = useState({quote: '', author: ''});
+  const [randomQuote, setRandomQuote] = useState({id: null, quote: '', author: ''});
+  const [reload, setReload] = useState(0);
 
   useEffect(() => {
     const loadRandomQuote = async () => {
-      const result = await db.getAllAsync('SELECT quote, author FROM quotes ORDER BY RANDOM() LIMIT 1');
-      setRandomQuote(result[0])
+      const result = await db.getAllAsync(`SELECT id, quote, author FROM quotes WHERE id!='${randomQuote.id}' ORDER BY RANDOM() LIMIT 1`);
+      if(result.length > 0) {
+        setRandomQuote(result[0])
+      }
+      else {
+        setRandomQuote({quote: `This could be your quote!`, author: 'InformatiKater'})
+      }
     };
 
     loadRandomQuote();
-  }, [])
+  }, [reload])
 
-  console.log(randomQuote)
+
+  const [menuVisible, setMenuVisible] = useState(false);
+  const menuOpacity = useAnimatedValue(0);
+
+  const homeMenu = () => {
+    if(menuVisible) {
+      Animated.timing(menuOpacity, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }
+    else {
+      Animated.timing(menuOpacity, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }
+    
+    setMenuVisible(!menuVisible);
+    
+  };
+  
+  const scaleHomeMenu = useAnimatedValue(1);
+  const scaleMenu = useAnimatedValue(1);
+  const scaleAdd = useAnimatedValue(1);
+  const scaleReload = useAnimatedValue(1);
+
+  const pressInAnim = (scale) => {
+    Animated.timing(scale, {
+      toValue: 0.75,
+      duration: 100,
+      useNativeDriver: true
+    }).start();
+  };
+
+  const pressOutAnim = (scale) => {
+    Animated.timing(scale, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true
+    }).start();
+  };
+
+  const rotation = useAnimatedValue(0);
+  const rotationString = rotation.interpolate({
+    inputRange: [0, 360],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  const rotate = () => {
+    rotation.setValue(0);
+    Animated.timing(rotation, {
+      toValue: 359,
+      duration: 400,
+      useNativeDriver: true
+    }).start();
+  }
 
 
   return (
 
     <View style={styles.container}>
-      <View 
-        style = {styles.quoteContainer}
-      >
-        <Text style = {styles.quoteText}>
-          "{randomQuote.quote}"
-        </Text>
 
-        <Text style = {styles.authorText}>
-          - {randomQuote.author}
-        </Text>
+      <View style={styles.quoteSection}>
 
-      </View>
+        <Animated.View style={[styles.reload, {opacity: menuOpacity, transform: [{scale: scaleReload}, {rotate: rotationString}]}]}>
+            <Pressable disabled={!menuVisible} onPress={() => {setReload(prev => prev+1); rotate();}} onPressIn={()=>{pressInAnim(scaleReload)}} onPressOut={()=>{pressOutAnim(scaleReload)}}>
+                <Ionicons name="reload-circle" size={60} color={Colors.appGray.base} />
+            </Pressable>
+        </Animated.View>
 
-      <View style={styles.buttonContainer}>
-          <Pressable style={styles.button} onPress={() => navigation.navigate('menu')} >
-            <Text style={styles.buttonText}> Menu </Text>
+        <View style = {styles.quoteContainer}>
+          <Text style = {styles.quoteText} adjustsFontSizeToFit={true} numberOfLines={8}>
+            "{randomQuote.quote}"
+          </Text>
+          <Text style = {styles.authorText} adjustsFontSizeToFit={true} numberOfLines={2}>
+            - {randomQuote.author}
+          </Text>
+        </View>
+
+        <Animated.View style={{transform: [{scale: scaleHomeMenu}]}}>
+          <Pressable style={styles.open} onPress={homeMenu} onPressIn={()=>{pressInAnim(scaleHomeMenu)}} onPressOut={()=>{pressOutAnim(scaleHomeMenu)}}>
+              <Feather name="loader" size={40} color={Colors.appGray.base} />
           </Pressable>
-
-        <Pressable style={styles.button} onPress={() => navigation.navigate('quoteForm', {id: null})}>
-          <Text style={styles.buttonText}> Add </Text>
-        </Pressable>
+        </Animated.View>
+        
       </View>
+
+      <Animated.View style={{opacity: menuOpacity, flexDirection: 'row'}}>
+
+          <Animated.View style={[{transform: [{scale: scaleMenu}]}, styles.button]}>
+            <Pressable disabled={!menuVisible} onPress={() => navigation.navigate('menu')} onPressIn={()=>{pressInAnim(scaleMenu)}} onPressOut={()=>{pressOutAnim(scaleMenu)}} >
+                <Text style={styles.buttonText}> Menu </Text>
+            </Pressable>
+          </Animated.View>
+
+          <Animated.View style={[{transform: [{scale: scaleAdd}]}, styles.button]}>
+            <Pressable disabled={!menuVisible} onPress={() => navigation.navigate('quoteForm', {id: null})}onPressIn={()=>{pressInAnim(scaleAdd)}} onPressOut={()=>{pressOutAnim(scaleAdd)}}>
+              <Text style={styles.buttonText}> Add </Text>
+            </Pressable>
+          </Animated.View>
+
+      </Animated.View>
 
     </View>
 
@@ -65,9 +151,12 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingVertical: 50,
   },
+  quoteSection: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   quoteContainer: {
     ...quoteBoxStyle.quoteContainer,
-    marginTop: 200
   },
   quoteText: {
     ...quoteBoxStyle.quoteText
@@ -75,14 +164,11 @@ const styles = StyleSheet.create({
   authorText: {
     ...quoteBoxStyle.authorText
   },
-  buttonContainer: {
-    flexDirection: 'row',
-  },
   button: {
     height: 60,
     width: 120,
     borderRadius: 50,
-    backgroundColor: Colors.appGray.base05,
+    backgroundColor: Colors.appGray.base,
     justifyContent: "center",
     marginBottom: 50,
     marginHorizontal: 'auto'
@@ -92,5 +178,13 @@ const styles = StyleSheet.create({
     fontFamily: FontFamilies.baseFont,
     textAlign: 'center',
     fontSize: 25
+  },
+  reload: {
+    marginHorizontal: 'auto',
+    marginBottom: 20
+  },
+  open: {
+    marginHorizontal: 'auto',
+    marginTop: 20
   }
 })
